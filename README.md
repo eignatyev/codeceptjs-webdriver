2ddd5bddff41d9c1999c39a08f26f0c2e85b4476
Egor Ignatyev <egor.ignatyev@just-eat.com>
2020-08-30 14:51:37 +0100

    Fixed bugs and provided npm scripts

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
 # Web Automated Testing PoC with CodeceptJS and WebdriverIO
 
-## Preconditions
+## Prepare the Working Space
 
 1. Install [Node.js](https://nodejs.org/en/) (version `12.18.3 LTS`)
-1. Install [Docker Desktop](https://docs.docker.com/desktop/)
 1. Clone the repo `git clone https://github.com/eignatyev/codeceptjs-webdriver.git`
 1. Open the root folder `cd codeceptjs-webdriver`
 1. Install node modules `npm i`
 
-## Local Run Options
+## Run Locally With Selenium WebDriver
 
-1. Cross-browser run `npx codeceptjs run-multiple basic`
-1. Single-browser run `npx codeceptjs run-multiple basic:chrome` or `npx codeceptjs run-multiple basic:firefox`
+1. Cross-browser run `npm run test:cross`
+1. Single-browser run `npm run test:chrome` or `npm run test:firefox`
 
 ## Run with the Selenoid Docker image
 
+### Preconditions
+
+1. Install [Docker Desktop](https://docs.docker.com/desktop/)
 1. Start Docker Desktop locally
-1. Enable the Selenoid plugin in `codecept.conf.js`
-1. Disable the wdio plugin in `codecept.conf.js`
-1. Use the same commands as for local runs
+1. Enable the `Selenoid` plugin in `codecept.conf.js`
+1. Disable the `wdio` plugin in `codecept.conf.js`
+
+----
+
+1. Cross-browser run `npm run test:selenoid:cross`
+1. Single-browser run `npm run test:selenoid:chrome` or `npm run test:selenoid:firefox`
--- pages/menu_page.js
+++ pages/menu_page.js
@@ -4,21 +4,29 @@ module.exports = {
 
   submitButton: '.basket__order-button',
   mealNameLabel: locate('span.meal-name span'),
+  menuCardContainer: locate('#imenucard'),
   categoryContainer: locate('.menucard__meals-group'),
   categoryNameLabel: locate('.menucard__meals-group-category'),
 
   getCategoryContainer(categoryName) {
     return this.categoryContainer.withChild(this.categoryNameLabel.withText(categoryName));
   },
+
   async addMenuToBasket(menuName) {
-    const menuItemNames = await I.grabTextFrom(this.mealNameLabel.inside(this.getCategoryContainer(menuName)));
-    menuItemNames.forEach(n => this.addSimpleItemToBasket(n));
+    const categoryContainer = this.getCategoryContainer(menuName);
+    const menuItemsNames = await I.grabTextFrom(this.mealNameLabel.inside(categoryContainer));
+    menuItemsNames.forEach(n => this.addSimpleItemToBasket(n, categoryContainer));
   },
-  addSimpleItemToBasket(itemName) {
-    const menuItem = this.mealNameLabel.withText(itemName);
+
+  addSimpleItemToBasket(itemName, categoryContainer) {
+
+    let menuItem = this.mealNameLabel.withText(itemName);
+    categoryContainer = categoryContainer || this.menuCardContainer;
+    menuItem = menuItem.inside(categoryContainer);
     I.seeElement(menuItem);
     I.click(menuItem);
   },
+
   submitBasket() {
     I.click(this.submitButton);
   }
